c6c3f16d20f6c51f354e93c0440f674f564648bf
rasuliomari <rasuli707046@gmail.com>
2026-08-19 21:56:01 +0300

    The setting of environment

--- student-management/src/main/java/com/example/student_management/StudentManagementApplication.java
+++ student-management/src/main/java/com/example/student_management/StudentManagementApplication.java
@@ -1,13 +1,13 @@
-package com.example.student_management;
+// package com.example.student_management;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class StudentManagementApplication {
+// @SpringBootApplication
+// public class StudentManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(StudentManagementApplication.class, args);
+// 	}
 
-}
+// }
